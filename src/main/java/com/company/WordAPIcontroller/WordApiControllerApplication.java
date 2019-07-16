6806f6632309a8437972d5386fc5c806cc235530
Natalie Vasquez <natalievasquez11@gmail.com>
2019-07-16 10:59:37 -0500

    Initial commit

---
+++ src/main/java/com/company/WordAPIcontroller/WordApiControllerApplication.java
@@ -0,0 +1,13 @@
+package com.company.WordAPIcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordApiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordApiControllerApplication.class, args);
+	}
+
+}
